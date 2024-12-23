d78af76952d9b7772caeff1a81a634a2eeae690e
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-23 09:54:23 -0300

    Cerveja

--- src/main/java/DesignPattern_template_method/DesignPattern_template_method/Cerveja.java
+++ src/main/java/DesignPattern_template_method/DesignPattern_template_method/Cerveja.java
@@ -1,2 +1,21 @@
-package DesignPattern_template_method.DesignPattern_template_method;public class Cerveja {
+package DesignPattern_template_method.DesignPattern_template_method;
+
+import java.util.Random;
+
+public class Cerveja extends Bebida{
+    @Override
+    protected boolean possuiIngredientesParaBebida() {
+        System.out.println("Indo ao banco de dados e validando se possui iingredientes para cerveja!");
+        return new Random().nextBoolean();
+    }
+
+    @Override
+    protected void prepararIngredientes() {
+        System.out.println("Preparando ingrediente para o cerveja...");
+    }
+
+    @Override
+    protected void fazerBebida() {
+        System.out.println("Fazendo o cerveja...");
+    }
 }
