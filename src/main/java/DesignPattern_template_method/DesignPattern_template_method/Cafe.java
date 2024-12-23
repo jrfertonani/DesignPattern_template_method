77144670ac15bc9dc382ba093ecd0d16bffb6aaa
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-23 09:54:07 -0300

    Cafe

--- src/main/java/DesignPattern_template_method/DesignPattern_template_method/Cafe.java
+++ src/main/java/DesignPattern_template_method/DesignPattern_template_method/Cafe.java
@@ -1,2 +1,21 @@
-package DesignPattern_template_method.DesignPattern_template_method;public class Cafe {
+package DesignPattern_template_method.DesignPattern_template_method;
+
+import java.util.Random;
+
+public class Cafe extends Bebida{
+    @Override
+    protected boolean possuiIngredientesParaBebida() {
+        System.out.println("Indo ao banco de dados e validando se possui iingredientes para cafe!");
+        return new Random().nextBoolean();
+    }
+
+    @Override
+    protected void prepararIngredientes() {
+        System.out.println("Preparando ingrediente para o cafe...");
+    }
+
+    @Override
+    protected void fazerBebida() {
+        System.out.println("Fazendo o cafe...");
+    }
 }
