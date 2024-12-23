47eba8550044ce5491925bab62d692d1c850766e
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-23 09:54:42 -0300

    Principal

--- src/main/java/DesignPattern_template_method/DesignPattern_template_method/Principal.java
+++ src/main/java/DesignPattern_template_method/DesignPattern_template_method/Principal.java
@@ -1,2 +1,18 @@
-package DesignPattern_template_method.DesignPattern_template_method;public class Principal {
+package DesignPattern_template_method.DesignPattern_template_method;
+
+import org.springframework.boot.SpringApplication;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Bebida bebida = new Cerveja();
+        try{
+            bebida.prepararBebida();
+        }catch (RuntimeException ex){
+            System.out.println(ex.getMessage());
+        }
+        bebida.prepararBebida();
+    }
+
+
 }
