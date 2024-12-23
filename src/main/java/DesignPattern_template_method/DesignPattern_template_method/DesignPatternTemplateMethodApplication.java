102b8675a6c20316adfb84fe499adae805c13d32
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-23 09:30:10 -0300

    Start

---
+++ src/main/java/DesignPattern_template_method/DesignPattern_template_method/DesignPatternTemplateMethodApplication.java
@@ -0,0 +1,13 @@
+package DesignPattern_template_method.DesignPattern_template_method;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternTemplateMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternTemplateMethodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DesignPattern_template_method/DesignPattern_template_method/DesignPatternTemplateMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package DesignPattern_template_method.DesignPattern_template_method;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternTemplateMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
